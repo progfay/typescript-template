c34d29792688fbc2d2f7335011a9aa70098eb59e
progfay <shun1856shun@gmail.com>
2021-01-17 23:28:20 +0900

    feat: setup jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  moduleFileExtensions: ["ts", "js"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json",
+    },
+  },
+  collectCoverageFrom: ["src/**/*.ts"],
+  testMatch: ["**/tests/**/*.test.ts"],
+};
